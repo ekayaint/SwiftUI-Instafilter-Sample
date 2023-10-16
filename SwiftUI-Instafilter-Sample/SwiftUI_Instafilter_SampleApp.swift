e2724626a68e195b0627b8335cac6cdef1b21215
ekayaint <ekayaint@gmail.com>
2023-10-16 20:18:52 +0300

    Initial Commit

---
+++ SwiftUI-Instafilter-Sample/SwiftUI_Instafilter_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Instafilter_SampleApp.swift
+//  SwiftUI-Instafilter-Sample
+//
+//  Created by ekayaint on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Instafilter_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
